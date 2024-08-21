1530256f974c41d9e86c78e66d22be08a87bea8c
jucastro15 <ra56421610838@acaonsfatima.org.br>
2024-08-21 00:43:23 -0300

    Parte 1 JU quase pronta

---
+++ src/components/cabecalho/index.jsx
@@ -0,0 +1,24 @@
+import './index.scss';
+import { Link } from 'react-router-dom';
+
+
+export default function Cabecalho() {
+    
+    return(
+        
+        <header className="cabeçalho">
+            <div className="imh1">
+                <img className="femme" src="/assets/images/logo.femme.png" alt="Femme" />
+                <h1>Femme</h1>
+            </div>
+
+            <nav>
+                <ul>
+                    <li><Link to='/'>Inicio</Link></li>
+                    <li><Link to='/sobre'>Sobre Femme</Link></li>
+                </ul>
+            </nav>
+
+        </header>
+    );
+}
--- src/pages/femme/index.jsx
+++ src/pages/femme/index.jsx
@@ -1,27 +1,45 @@
+import Cabecalho from '../../components/cabecalho'
 import './index.scss'
 
 export default function Femme() {
     return (
-        <div className='pagina-femme'>
-          
+        <div className='pagina-femme pagina'>
+            <Cabecalho />
+            <section className='unik'>
+                <div className="hero">
+                    <div className="hero-text">
+                        <h1>Junte-se a Femme e descubra o melhor da tecnologia</h1>
+                    </div>
+                    <div className="hero-image">
+                        <img src="/assets/images/mulher1.png" alt="mulher1" />
+                    </div>
+                </div>
+                <div className="sobre">
+                    <div className="sobre-con">
+                        <div className="sobre-imh2">
+                            <img  className = 'femme' src="/assets/images/logo.femme.png" alt="Femme Logo" />
+                            <h2>Sobre Femme</h2>
+                        </div>
+                        <p>
+                            Femme é uma equipe dinâmica e criativa composta por quatro talentosas desenvolvedoras de sites: Júlia, Maria, Mariana e Rayane. Juntas, formamos uma força inovadora no desenvolvimento de soluções digitais que não apenas atendem às necessidades de nossos clientes, mas também superamos suas expectativas.
+                        </p>
+                    </div>
+                </div>
+            </section>
+
+
             <section>
-                
+
+
 
 
             </section>
 
-            <section>
-
-
-               
-
-            </section>
-
 
             <section>
 
 
-                
+
 
 
             </section>
@@ -29,9 +47,9 @@ export default function Femme() {
             <section className='desenvolvedoras'>
 
             </section>
-  
+
             <footer>
-                
+
             </footer>
         </div>
     )
