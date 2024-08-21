7ec465545c5b28bd005332b442b5dc1eba0c08eb
jucastro15 <ra56421610838@acaonsfatima.org.br>
2024-08-20 21:46:38 -0300

    router

---
+++ src/router.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Femme from './pages/femme';
+
+
+export default function Navegacao() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='/' element={<Femme />} />
+                
+
+            </Routes>
+        </BrowserRouter>
+    );
+}
